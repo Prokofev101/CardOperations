293bdfe662e89a12519439a1c31dd7404031513b
Valery Prokofev <vvvpvv1991@gmail.com>
2022-06-28 16:57:07 +0400

    corrected private to public + задание 2

--- src/ru/geekstar/Main.java
+++ src/ru/geekstar/Main.java
@@ -12,6 +12,7 @@ public class Main {
         visaCard.pay(100.50f);
         visaCard.pay(250.00f);
         visaCard.transfer(100.00f);
+        visaCard.depositing(300.00f);
 
         //System.out.println("\nОперации по карте " + visaCard.getPaySystem() + " " +visaCard.getNumberCard() + ": ");
         String[] transactions = visaCard.getTransactions();
